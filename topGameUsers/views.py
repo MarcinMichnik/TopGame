660e4744aa5254966863a5de258c64866c1b096e
Marcin Michnik <m.michnik.272@studms.ug.edu.pl>
2021-06-20 00:36:15 +0200

    :sparkles: user app v1

--- topGameUsers/views.py
+++ topGameUsers/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.contrib.auth import login
+from django.shortcuts import redirect, render
+from django.urls import reverse
+from django.contrib.auth.models import User
+from users.forms import CustomUserCreationForm
 
-# Create your views here.
+def register(request):
+    if request.method == "GET":
+        return render(
+            request, "users/register.html",
+            {"form": CustomUserCreationForm}
+        )
+    elif request.method == "POST":
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect(reverse("dashboard"))
