529b18a3b9bfddcfbb0c7aeb63f16b171b071ce8
Marcin Michnik <m.michnik.272@studms.ug.edu.pl>
2021-06-20 13:56:07 +0200

    :sparkles: user profiles

--- topGameUsers/views.py
+++ topGameUsers/views.py
@@ -1,8 +1,21 @@
 from django.contrib.auth import login
 from django.shortcuts import redirect, render
 from django.urls import reverse
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.http import HttpResponseRedirect
+from django.core.exceptions import ValidationError   
+from django.db.models import Sum
+from datetime import timedelta, datetime
+from django.utils import timezone 
+from django.db.models import Count
+from django.views.generic import (TemplateView,
+                                       ListView,
+                                       DetailView,
+                                       UpdateView,
+                                       CreateView,
+                                       DeleteView)
 from django.contrib.auth.models import User
-from .forms import CustomUserCreationForm
+from django.contrib.auth.decorators import login_required
 
 def register(request):
     if request.method == "GET":
@@ -16,3 +29,24 @@ def register(request):
             user = form.save()
             login(request, user)
             return redirect(reverse("dashboard"))
+        
+@login_required
+def profile(request):
+    if request.method == 'POST':
+        u_form = UserUpdateForm(request.POST, instance=request.user)
+        p_form = ProfileUpdateForm(request.POST, request.FILES, instance=request.user.profile)
+        if u_form.is_valid() and p_form.is_valid():
+            u_form.save()
+            p_form.save()
+            messages.success(request, f'Konto zostało zaktualizowane.')
+            return redirect('profile')
+    else:
+        u_form = UserUpdateForm(instance=request.user)
+        p_form = ProfileUpdateForm(instance=request.user.profile)
+        
+    context = {
+        'u_form':u_form,
+        'p_form':p_form
+    }
+    
+    return render(request, 'topGameUsers/profile.html', context)
