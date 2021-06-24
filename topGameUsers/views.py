e98a9716563ad46e5cd09d68c8491e985b9210c3
Marcin Michnik <m.michnik.272@studms.ug.edu.pl>
2021-06-24 20:48:23 +0200

    :sparkles: current user mana sum statistic

--- topGameUsers/views.py
+++ topGameUsers/views.py
@@ -60,31 +60,37 @@ class UserStatistics(TemplateView):
     template_name = 'topGameUsers/statistics.html'
     
     def get(self, request, *args, **kwargs):
-        
-        def getUserManaRecords(self, request):
-            result = []
-            timezone_index = timezone.now() + timedelta(days=-11)
-            for i in range(11):
-                timezone_index += timedelta(days=1)
-                manas = Mana.objects.filter(belongs_to=request.user, date_of_assignment__lte=timezone_index)
-                
-                x_value = str(timezone_index.date())
-                y_value = manas.aggregate(power_sum=Sum('power'))['power_sum']
-                
-                if y_value is None:
-                    y_value = 0
-                
-                result.append({'x':x_value, 'y':y_value}) 
-                
-            return result
-            
-        
-        user_manas = getUserManaRecords(self, request)
-        
         context = {}
+        
+        user_manas = self.getUserManaRecords(request)
         context['user_manas'] = user_manas
+        
+        current_mana_sum = self.getCurrentUserManaSum(request)
+        context['current_mana_sum'] = current_mana_sum
         
         #import pdb; pdb.set_trace()
 
         return render(request, self.template_name, context)
     
+    def getUserManaRecords(self, request):
+        result = []
+        timezone_index = timezone.now() + timedelta(hours=5) + timedelta(days=-11)
+        for i in range(11):
+            timezone_index += timedelta(days=1)
+            manas = Mana.objects.filter(belongs_to=request.user, date_of_assignment__lte=timezone_index)
+            
+            x_value = str(timezone_index.date())
+            y_value = manas.aggregate(power_sum=Sum('power'))['power_sum']
+            
+            if y_value is None:
+                y_value = 0
+            
+            result.append({'x':x_value, 'y':y_value}) 
+            
+        return result
+    
+    def getCurrentUserManaSum(self, request):
+        current_user_manas = Mana.objects.filter(belongs_to=request.user)
+        current_user_mana_sum = current_user_manas.aggregate(all_user_power_sum=Sum('power'))['all_user_power_sum']
+        
+        return current_user_mana_sum
